c1b9f479f25152b24fdab9baba9da042e6a177a6
zm1hawk <vinicius6.lopes6@gmail.com>
2022-09-27 11:24:25 -0300

    aula 27-09-22 CriandoReactBlogPessoal

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div className="App">
+      <h1 className="c1">Call Of Duty Warzone</h1>
+        <h3>O que é ? Como e onde jogar ? Acompanhe a leitura para entender mais sobre este jogaço! </h3>
+    </div>
+  );
+}
+
+export default App;
